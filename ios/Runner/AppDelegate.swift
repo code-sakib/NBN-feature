c6de1355f2e21ca6b58220ccd2a60a185f20e7d6
sakib-shaikh-github <sakibshaikh1507@gmail.com>
2025-05-28 15:21:44 +0530

    retreiving api keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyB9CSCdASYKtmldOpr2FcqfMNQhfFgRCGQ")
+    GMSServices.provideAPIKey("api-key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
